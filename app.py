9a4689f912deca2b8297c657fb7cdacac7e1b40c
vagano <vagano@gmail.com>
2018-06-24 17:16:09 +0500

    ptz presets dictionary

--- app.py
+++ app.py
@@ -48,8 +48,8 @@ def get_presets_list():
                 token_name = str(p.Name)
                 presets_json[token] = {
                     "token_name": token_name,
-                    "name_ru": presets_dict[token_name]['ru'],
-                    "name_en": presets_dict[token_name]['en']
+                    "name_ru": unicode(presets_dict[token_name]['ru'],"utf-8"),
+                    "name_en": unicode(presets_dict[token_name]['en'],"utf-8")
                 }
                 return str(presets_json)
         except Exception as e:
