b8f114efd103413fcdff2e57a4db9634550a6704
vagano <vagano@gmail.com>
2018-06-24 13:47:33 +0500

    app logger introduced

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import logging
+
 from flask import Flask, g, jsonify
 from ptzcam import PtzCam
 
@@ -5,18 +7,26 @@ app = Flask(__name__)
 
 
 def _get_camera():
-    cam = getattr(g, '_cam', None)
-    if cam is None:
-        cam = g._cam = PtzCam('192.168.1.110', 8999, 'admin', '', '/home/pi/camApi/venv/wsdl/')
-    return cam
+    try:
+        cam = getattr(g, '_cam', None)
+        if cam is None:
+            cam = g._cam = PtzCam('192.168.1.110', 8999, 'admin', '', '/home/pi/camApi/venv/wsdl/')
+        return cam
+    except Exception as e:
+        app.logger.error(str(e))
+        return '500'
 
 
 def _get_presets():
-    presets = getattr(g, '_presets', None)
-    if presets is None:
-        cam = _get_camera()
-        presets = g._presets = jsonify(cam.get_presets())
-    return presets
+    try:
+        presets = getattr(g, '_presets', None)
+        if presets is None:
+            cam = _get_camera()
+            presets = g._presets = jsonify(cam.get_presets())
+        return presets
+    except Exception as e:
+        app.logger.error(str(e))
+        return '500'
 
 
 @app.route('/cam/api/get_presets_list/')
